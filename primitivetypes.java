0f6e4dc09c68a97aa26dea372df8cffffcd28675
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-23 16:40:11 +0530

    Add files via upload
    learn various data types in java

---
+++ primitivetypes.java
@@ -0,0 +1,39 @@
+package com.itbulls.ramu;
+
+public class primitivetypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=204507000;
+		float f=9.9999f;
+        char c=109;
+        char d='o';
+        boolean bol=true;
+        byte mynum1 = 123;
+        byte mynum = (byte)129;
+        short s1=1234;
+        short s=(short)80000;
+        long k = 10000000000l;
+        int s2=80000-32767;
+        double j= 19.99999989999;
+        double g=34e5;
+        float p = 78e4f;
+        int r = mynum;
+        byte t = (byte)m;
+        float approx =m;
+        System.out.println(mynum);
+        System.out.println(mynum1);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1);
+        System.out.println(f);
+        System.out.println(k);
+        System.out.println(j);
+        System.out.println(g);
+        System.out.println(p);
+        System.out.println(r);
+        System.out.println(t);
+        System.out.println(m-(int)approx);
+	}
+
+}
