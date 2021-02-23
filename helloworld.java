134b64230761bc1a6e87017215eefe9971e7f34d
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-23 16:38:27 +0530

    Add files via upload
    Basic java program to print hello world

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+package com.itbulls.ramu;
+
+public class helloworld {
+	/**
+	 * the entry point to the program
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * please do not remove this code
+		 * this is my kind request
+		 */
+       System.out.println("Hello World!");
+//       System.out.println("Hello World!");
+       //TODO add more code here in the future
+	}
+
+}
