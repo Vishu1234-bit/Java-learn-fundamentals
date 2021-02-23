62cf827052d05b46f8e7f1379ffde19b67417908
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-23 16:40:51 +0530

    Add files via upload
    learn how to represent each number system in java

---
+++ numsys.java
@@ -0,0 +1,16 @@
+
+public class numsys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dec = 152;
+		int bin  =0b100;
+		int oct = 02576666;
+		int hex = 0x56AF;
+		System.out.println(dec);
+		System.out.println(bin);
+		System.out.println(oct);
+		System.out.println(hex);
+	}
+
+}
