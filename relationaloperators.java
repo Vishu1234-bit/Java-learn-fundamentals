4afc2518f8f825d19ae0b25463bf41f3ce7694f4
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-25 20:39:41 +0530

    Add files via upload

---
+++ relationaloperators.java
@@ -0,0 +1,18 @@
+
+public class relationaloperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 19;
+		int b = 30;
+		System.out.println("a==b "+(a==b));
+	    System.out.println("b!=a "+(b!=a));
+	    System.out.println("a>=b "+(a>=b));
+	    System.out.println("a<=b "+(a<=b));
+	    System.out.println("a>b "+(a>b));
+	    System.out.println("a<b "+(a<b));
+	   
+
+	}
+
+}
