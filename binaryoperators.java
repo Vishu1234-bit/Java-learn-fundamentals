d81d06203114e8ce0182a8ec75b7e39aa2bd4349
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-25 20:18:01 +0530

    Add files via upload

---
+++ binaryoperators.java
@@ -0,0 +1,24 @@
+
+public class binaryoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//binaryoperators + / * % -
+		int a = 4;
+		int b = 8;
+		int c = a+b;
+		int d = a-b;
+		int e = a*b;
+		int f = a/b;
+		int g = a%b;
+	    System.out.println("a="+a);
+	    System.out.println("b="+b);
+	    System.out.println("c="+c);
+	    System.out.println("d="+d);
+	    System.out.println("e="+e);
+	    System.out.println("f="+f);
+	    System.out.println("g="+g);
+
+	}
+
+}
