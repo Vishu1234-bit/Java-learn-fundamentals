b2fb2b259766a1c523c547922a0b0c8beeb82b9e
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-25 20:12:02 +0530

    Add files via upload

---
+++ unaryoperators.java
@@ -0,0 +1,25 @@
+package example;
+
+public class refactoring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//unary operators =  +   -   ++   --
+		int i = 4;
+		int a = i++;
+		int b = ++i;
+		int c = i--;
+		int d = --i;
+		int e = -i;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+		System.out.println("e="+e);
+		
+		
+		
+		
+
+	}
+}
