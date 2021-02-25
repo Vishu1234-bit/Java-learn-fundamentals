9eaec127cb0272299fb3ac05871702520118e060
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-25 20:34:22 +0530

    Add files via upload

---
+++ assignmentoperators.java
@@ -0,0 +1,19 @@
+
+public class assignmentoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//assignmentoperators += -= /= %= = *=
+		int a = 6;
+		int b = 4;
+		a+=b;
+		a-=b;
+		a/=b;
+		a%=b;
+		a*=b;
+		System.out.println("a="+a);
+	    System.out.println("b="+b);
+	    
+	}
+
+}
