683e7418cc1d4152f71f194b4bc246bf4b074f30
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-26 16:07:34 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,23 @@
+import compiler.loop.MathUtil;
+
+public class math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.PI);
+		System.out.println(Math.max(4, 9));
+		System.out.println(Math.E);
+		System.out.println(Math.min(7, 3));
+		System.out.println(Math.abs(-7));
+		System.out.println(Math.sqrt(6));
+		System.out.println(Math.sqrt(-5));
+		System.out.println(9/0.0);
+		System.out.println(-8/0.0);
+		System.out.println(Math.round(6.0/5.8)*7/6);
+		System.out.println(Math.random()); //between 0.0 and 1.0
+		System.out.println(Math.random()*100);//between 0 and 100
+		System.out.println((int)(Math.random()*100));// all whole numbers between 0 and 100
+		System.out.println((Math.random()*100)+100);//between 100 and 200
+	}
+
+}
