811604664023ef7e091fd3d122bfba280d8e744b
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-02-26 13:56:35 +0530

    Add files via upload

---
+++ ternaryoperators.java
@@ -0,0 +1,17 @@
+
+public class ternaryoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 9;
+        int b = 0b101010011010;
+        String c = (2>=1?"yes":"no");
+        String d = (a>=(byte)b?"true":"false");       
+        System.out.println((byte)b);
+        System.out.println(d);
+        
+        System.out.print(c);
+        
+	}
+
+}
