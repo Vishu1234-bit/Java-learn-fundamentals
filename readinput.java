4f2157466049fcf6760c2041a7314e9771e5bfd2
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-03-01 13:22:31 +0530

    Add files via upload

---
+++ readinput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class readinput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any string:");
+		String s = sc.next();
+		System.out.println("The user entered:"+s);
+		System.out.println("Enter any integer:");
+		int i = sc.nextInt();
+		System.out.println("The user entered:"+i);
+
+	}
+
+}
