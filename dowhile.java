49e91489c077ed3bb7274d8625c7ff20459e4494
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-03-07 15:50:21 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,16 @@
+
+public class dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter=0;
+		do {
+			System.out.println("counter: " + (counter++));
+		}while(counter<7);
+		do {
+			System.out.println("counter is seven already");
+		}while(counter<7);
+
+	}
+
+}
