4991cc132202afd8e14dbe319a55871e0f9addb8
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2021-03-07 15:28:13 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,14 @@
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter=0;
+		while(counter<5) {
+			System.out.println("Hello Vishali");
+			counter+=1;
+		}
+
+	}
+
+}
